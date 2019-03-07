562af6e2cbf5a7f1b84ba60f4281030adb7a1f33
Khoa Mai <maidinhkhoa92@gmail.com>
2019-03-07 13:05:07 +0700

    fix image height

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -4,19 +4,19 @@ const footer = () => {
   return (
     <div className="footer-wrapper fixed-bottom">
       <a className="col-3" href="#">
-        <Icon name="mua-ban" height="53" />
+        <Icon name="mua-ban" height="35" />
         Mua bán
       </a>
       <a className="col-3" href="#">
-        <Icon name="san-pham" height="53" />
+        <Icon name="san-pham" height="35" />
         Sản phẩm
       </a>
       <a className="col-3" href="#">
-        <Icon name="danh-muc" height="53" />
+        <Icon name="danh-muc" height="35" />
         Danh mục
       </a>
       <a className="col-3" href="#">
-        <Icon name="mua-ban" height="53" />
+        <Icon name="mua-ban" height="35" />
         Quản lý
       </a>
     </div>
