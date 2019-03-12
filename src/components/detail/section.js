3b454f5e169a2cd00a9421bd837155bc44147a10
Khoa Mai <maidinhkhoa92@gmail.com>
2019-03-12 15:42:48 +0700

    Update detail coin

---
+++ src/components/detail/section.js
@@ -0,0 +1,155 @@
+import React, { Fragment, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import DatePicker from 'react-datepicker';
+
+const Section1 = () => {
+  return (
+    <div className="section">
+      <div className="row">
+        <div className="col-12">
+          <h3>
+            NVL1235860 <img src="/img/popup-icon.png" />
+          </h3>
+          <div className="row">
+            <div className="col-6">
+              <h4>Ngày đáo hạn</h4>
+              <h5>24/10/2019</h5>
+            </div>
+            <div className="col-6 text-right">
+              <h4>Hạn mức</h4>
+              <h5>
+                <span>23.659</span> trái phiếu
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Section2 = () => {
+  return (
+    <div className="section-2">
+      <div className="form-group row">
+        <label className="col-6 col-form-label">Ngày giao dịch</label>
+        <div className="col-6">
+          <DatePicker />
+        </div>
+      </div>
+      <div className="form-group row">
+        <label className="col-6 col-form-label">Giá đơn mua</label>
+        <div className="col-6">
+          <div className="input-group">
+            <input type="text" className="form-control" />
+            <div className="input-group-append">
+              <div className="input-group-text">VND</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="form-group row">
+        <label className="col-sm-6 col-form-label">Số lượng TP mua</label>
+        <div className="col-sm-6">
+          <div className="input-group number-field">
+            <button className="btn btn-light">+</button>
+            <input type="number" className="form-control" />
+            <button className="btn btn-light">-</button>
+          </div>
+        </div>
+      </div>
+      <div className="form-group sum-field row">
+        <label className="col-6 col-form-label">GIÁ TRỊ ĐẦU TƯ</label>
+        <div className="col-6">
+          <div className="input-group">
+            <input type="text" className="form-control" disabled value="510,760,000" />
+            <div className="input-group-append">
+              <div className="input-group-text">VND</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Section3 = () => {
+  return (
+    <div>
+      <div className="form-group row align-items-center">
+        <label className="col-6 col-form-label">Lãi suất đáo hạn</label>
+        <div className="col-6 text-right col-form-label">
+          <span>8</span>
+          <small>%/năm</small>
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label className="col-6 col-form-label">Thời gian nắm giữ</label>
+        <div className="col-6 text-right col-form-label">
+          <small>365 ngày</small>
+        </div>
+      </div>
+      <div className="form-group row align-items-center">
+        <label className="col-6 col-form-label">Lãi suất đầu tư</label>
+        <div className="col-6 text-right col-form-label">
+          <span>7,2</span>
+          <small>%/năm</small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Section4 = ({ title, onClick, status, refs }) => {
+  return (
+    <Fragment>
+      <div className="row">
+        <div className="col-12">
+          {title} <span className="color">217,377,499 VND</span>
+          <span onClick={() => onClick(refs)} className="float-right collapse-custom">
+            {status ? '+' : '-'}
+          </span>
+        </div>
+      </div>
+      {status && (
+        <div className="row">
+          <div className="col-12">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Nội dung</th>
+                  <th scope="col">Ngày thanh toán</th>
+                  <th scope="col">Tiền nhận</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Coupon</td>
+                  <td>02/04/2019</td>
+                  <td>8,600,0000</td>
+                </tr>
+                <tr>
+                  <td>Coupon</td>
+                  <td>02/04/2019</td>
+                  <td>8,600,0000</td>
+                </tr>
+                <tr>
+                  <td>Giá trị KH nhận được cuối kỳ</td>
+                  <td>02/04/2019</td>
+                  <td>8,600,0000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+Section4.propTypes = {
+  title: PropTypes.string,
+  onClick: PropTypes.func,
+  status: PropTypes.bool,
+  refs: PropTypes.string
+};
+export { Section1, Section2, Section3, Section4 };
--- src/container/bonds/detail.js
+++ src/container/bonds/detail.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import DatePicker from "react-datepicker";
 import Layout from '../layout/layout';
 import Popup from '../../components/common/popup';
+import { Section1, Section2, Section3, Section4 } from '../../components/detail/section';
 
 class Detail extends Component {
   constructor(props) {
     super(props);
     this.state = {
       toggle: {
-        popup: false
+        popup: false,
+        table1: true,
+        table2: false
       },
       startDate: new Date()
     };
   }
-  showPopup() {
+  showPopup = type => {
     this.setState({
       toggle: {
-        popup: !this.state.toggle.popup
+        ...this.state.toggle,
+        [type]: !this.state.toggle[type]
       }
     });
-  }
+  };
   componentDidMount() {}
   render() {
     return (
       <Layout title="Chi Tiết Sản phẩm">
-        <div className="white-bg bond-detail">
+        <div className="bond-detail">
           {this.state.toggle.popup && (
             <Popup title="Thông tin trái phiếu">
               <ul className="list-group list-group-flush">
@@ -36,161 +39,26 @@ class Detail extends Component {
               </ul>
             </Popup>
           )}
-          <div className="row section">
-            <div className="col-12">
-              <h3>
-                NVL1235860 <img src="/img/popup-icon.png" />
-              </h3>
-              <div className="row">
-                <div className="col-6">
-                  <h4>Ngày đáo hạn</h4>
-                  <h5>24/10/2019</h5>
-                </div>
-                <div className="col-6 text-right">
-                  <h4>Hạn mức</h4>
-                  <h5>
-                    <span>23.659</span> trái phiếu
-                  </h5>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Section1 />
           <form>
-            <div className="section-2">
-              <div className="form-group row">
-                <label className="col-6 col-form-label">Ngày giao dịch</label>
-                <div className="col-6">
-                  <DatePicker selected={this.state.startDate}/>
-                </div>
-              </div>
-              <div className="form-group row">
-                <label className="col-6 col-form-label">Giá đơn mua</label>
-                <div className="col-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" />
-                    <div className="input-group-append">
-                      <div className="input-group-text">VND</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-group row">
-                <label className="col-sm-2 col-form-label">Số lượng TP mua</label>
-                <div className="col-sm-10">
-                  <div className="input-group">
-                    <input type="number" className="form-control" />
-                  </div>
-                </div>
-              </div>
-              <div className="form-group sum-field row">
-                <label className="col-6 col-form-label">GIÁ TRỊ ĐẦU TƯ</label>
-                <div className="col-6">
-                  <div className="input-group">
-                    <input type="text" className="form-control" disabled value="510,760,000" />
-                    <div className="input-group-append">
-                      <div className="input-group-text">VND</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label for="staticEmail" className="col-sm-2 col-form-label">
-                Lãi suất đáo hạn
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  readOnly
-                  className="form-control-plaintext"
-                  id="staticEmail"
-                  value="8%"
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-2 col-form-label">Thời gian nắm giữ</label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  className="form-control-plaintext"
-                  id="staticEmail"
-                  value="365 7.2% / năm"
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-2 col-form-label">Lãi suất đầu tư</label>
-              <div className="col-sm-10">
-                <input
-                  type="text"
-                  readOnly
-                  className="form-control-plaintext"
-                  id="staticEmail"
-                  value="email@example.com"
-                />
-              </div>
-            </div>
+            <Section2 />
+            <Section3 />
           </form>
-          <h3>TỔNG TIỀN NHẬN ĐƯỢC (DỰ KIẾN)</h3>
-          <div className="row">
-            <div className="col-sm-2">Chưa bao gồm tái đầu tư coupon:</div>
-            <div className="col-sm-10">217,377,499VNĐ</div>
+          <div className="form-group sum-field row">
+            <label className="col-12 col-form-label">TỔNG TIỀN NHẬN ĐƯỢC (DỰ KIẾN)</label>
           </div>
-          <div className="row">
-            <div className="col-12">
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Nội dung</th>
-                    <th scope="col">Ngày thanh toán</th>
-                    <th scope="col">Tiền nhận</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Coupon</td>
-                    <td>02/04/2019</td>
-                    <td>8,600,0000</td>
-                  </tr>
-                  <tr>
-                    <td>Coupon</td>
-                    <td>02/04/2019</td>
-                    <td>8,600,0000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-2">Đã bao gồm tái đầu tư coupon:</div>
-            <div className="col-sm-10">217,377,499VNĐ</div>
-          </div>
-          <div className="row">
-            <div className="col-12">
-              <table className="table table-striped">
-                <thead>
-                  <tr>
-                    <th scope="col">Nội dung</th>
-                    <th scope="col">Ngày thanh toán</th>
-                    <th scope="col">Tiền nhận</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Coupon</td>
-                    <td>02/04/2019</td>
-                    <td>8,600,0000</td>
-                  </tr>
-                  <tr>
-                    <td>Coupon</td>
-                    <td>02/04/2019</td>
-                    <td>8,600,0000</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+          <Section4
+            title="Chưa bao gồm tái đầu tư coupon:"
+            status={this.state.toggle.table1}
+            refs="table1"
+            onClick={this.showPopup}
+          />
+          <Section4
+            title="Đã bao gồm tái đầu tư coupon:"
+            status={this.state.toggle.table2}
+            refs="table2"
+            onClick={this.showPopup}
+          />
           <button type="button" className="btn btn-primary btn-lg btn-block">
             Đặt lệnh mua
           </button>
