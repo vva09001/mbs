a27758de735d13782ade01f88db9f92be36781d0
Khoa Mai <maidinhkhoa92@gmail.com>
2019-04-19 17:29:39 +0700

    Fix icon

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import Icon from './icon';
-const footer = () => {
+const footer = ({active}) => {
   const { t } = useTranslation();
+  const datas = [
+    {
+      link: '/',
+      text: 'Mua bán',
+      icon: 'ic_buySale'
+    },
+    {
+      link: '/buy/',
+      text: 'Sản phẩm',
+      icon: 'ic_product'
+    },
+    {
+      link: '/user/',
+      text: 'Tài sản',
+      icon: 'ic_assets'
+    },
+    {
+      link: '/trade/',
+      text: 'Quản lý GD',
+      icon: 'ic_tradingM'
+    }
+  ];
   return (
     <div className="footer-wrapper fixed-bottom">
-      <Link to="/">
-        <Icon name="ic_buySale" height="35" />
-        {t('Mua bán')}
-      </Link>
-      <Link to="/buy/">
-        <Icon name="ic_product" height="35" />
-        {t('Sản phẩm')}
-      </Link>
-      <Link to="/user/">
-        <Icon name="ic_assets" height="35" />
-        {t('Tài sản')}
-      </Link>
-      <Link to="/trade/">
-        <Icon name="ic_tradingM" height="35" />
-        {t('Quản lý GD')}
-      </Link>
+      {_.map(datas, (item, index) => (
+        <Link key={index} to={item.link} className={active === item.link && 'active'}>
+          <Icon name={item.icon} height="35" />
+          {t(item.text)}
+        </Link>
+      ))}
     </div>
   );
 };
