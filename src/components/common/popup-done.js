089c64251aa147f81594aed6655b8c00b211364e
Khoa Mai <maidinhkhoa92@gmail.com>
2019-04-22 15:11:48 +0700

    Fixed: Update popup.

---
+++ src/components/common/popup-done.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+const popup = props => {
+  const { t } = useTranslation();
+  return (
+    <div className="popup">
+      <div className="popup-wrapper rounded bg-white position-relative">
+        <div className="popup-header mb-2 text-center">
+          <img src="/img/popup-title.png" alt="popup-title" />
+        </div>
+        <div className="popup-body mb-2 text-center">{props.children}</div>
+        <div className="popup-footer text-center">
+          <button type="button" onClick={props.showClosePopup} className="mclose">
+            {t('ĐÓNG')}
+          </button>
+          <button type="button" onClick={props.showViewPopup} className="mclose">
+            {t('XEM')}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+popup.propTypes = {
+  showClosePopup: PropTypes.func,
+  showViewPopup: PropTypes.func,
+  children: PropTypes.node
+};
+export default popup;
