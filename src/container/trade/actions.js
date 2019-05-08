563a8e5bdfc0937863337525419a826a13e675ca
vva0901 <haidangvu07@gmail.com>
2019-05-08 09:55:46 +0700

    fixed

--- src/container/trade/actions.js
+++ src/container/trade/actions.js
@@ -197,7 +197,7 @@ class Actions extends Component {
               </div>
               <div className="col-6 mdata">
                 <i>
-                  {Math.round(currency(info.taxValue))} {t('VNĐ')}
+                  {Math.round(info.taxValue)} {t('VNĐ')}
                 </i>
               </div>
             </div>
